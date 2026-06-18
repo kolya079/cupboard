000e07ffc5979ee7c69f00c38edec3a97972d2e1
someaddons <someaddons@github.com>
2026-06-18 22:28:17 +0200

    26.2

--- src/main/java/com/cupboard/mixin/ConfigScreenMixin.java
+++ src/main/java/com/cupboard/mixin/ConfigScreenMixin.java
@@ -1,14 +1,14 @@
 package com.cupboard.mixin;
 
 import com.cupboard.compat.DummyConfigScreen;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(Gui.class)
 public class ConfigScreenMixin
 {
     @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
